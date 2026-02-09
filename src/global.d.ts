e4102066eae93ef7f56b26db900abcb044518070
marciely <marciely.tech@gmail.com>
2026-02-09 11:02:29 -0300

    chore(types): remove legacy global declarations

--- src/global.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "*.module.css" {
-  const classes: Record<string, string>;
-  export default classes;
-}
-
-// Flag de desenvolvimento definida pelo bundler
-declare const __DEV__: boolean;
-
